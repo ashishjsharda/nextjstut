f53a2cb0f0c51dc11ffe63ab16e56151d89f1d9b
Ashish Sharda <ashishjsharda@gmail.com>
2022-12-12 06:22:20 -0600

    Add link

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,6 +1,11 @@
+import Link  from "next/link";
+
 export default function FirstPost(){
     return<div>
         <h1>First Post</h1>
         <p> Example para</p>
+        <h2>
+            <Link href="/">Back to home</Link>
+        </h2>
     </div>
 }
